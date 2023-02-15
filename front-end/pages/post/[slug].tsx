0295d052ba80d80cb3962f2235323431b43e0996
bohdanloik-n <bohdanloik@nerdzlab.com>
2023-02-15 22:07:22 +0200

    test commit

---
+++ front-end/pages/post/[slug].tsx
@@ -0,0 +1,41 @@
+// [slug].tsx
+
+import client from '../../client'
+
+const Post = (props:any) => {
+    const { title = 'Missing title', name = 'Missing name' } = props.post
+    console.log(props, 'props')
+    return (
+        <article>
+            <h1>{title}</h1>
+            <span>By {name}</span>
+
+        </article>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = await client.fetch(
+        `*[_type == "post" && defined(slug.current)][].slug.current`
+    )
+
+    return {
+        paths: paths.map((slug:any) => ({params: {slug}})),
+        fallback: true,
+    }
+}
+
+export async function getStaticProps(context:any) {
+    // It's important to default the slug so that it doesn't return "undefined"
+    const { slug = "" } = context.params
+    const post = await client.fetch(`
+    *[_type == "post" && slug.current == $slug][0]{title, "name": author->name}
+  `, { slug })
+    return {
+        props: {
+            post
+        }
+    }
+}
+
+export default Post
